6c69673404734ec8c802358658cf885a1938ec5f
tlx02 <e0774379@u.nus.edu>
2024-09-06 22:57:19 +0800

    Add event listeners to number buttons, to update the display

--- javascript.js
+++ javascript.js
@@ -21,3 +21,22 @@ let operator;
 function operate(val1, operator, val2) {
     return operator(val1, val2);
 }
+
+let displayString = "";
+
+values = document.querySelectorAll(".value");
+display = document.querySelector(".display");
+
+
+for (let i = 0; i < values.length; i++) {
+    const node = values[i];
+    node.addEventListener("click", () => {
+        displayString += node.textContent;
+        display.textContent = displayString;
+    });
+}
+
+// buttonNames = []
+// let displayString = "";
+
+// for (let )
