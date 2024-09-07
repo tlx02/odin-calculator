16454a9305985eeffb0e0c6413dc7fecd980e27a
tlx02 <e0774379@u.nus.edu>
2024-09-07 11:28:23 +0800

    Link css elements to javascript to make the calculator work

--- javascript.js
+++ javascript.js
@@ -14,19 +14,20 @@ function divide(val1, val2) {
     return val1 / val2;
 }
 
+function operate(val1, operator, val2) {
+    return operator(Number(val1), Number(val2));
+}
+
 let firstNumber;
 let secondNumber;
 let operator;
 
-function operate(val1, operator, val2) {
-    return operator(val1, val2);
-}
-
-let displayString = "";
-
-values = document.querySelectorAll(".value");
+let displayString = " ";
 display = document.querySelector(".display");
 
+let values = document.querySelectorAll(".value");
+let operators = document.querySelectorAll(".operator");
+let equal = document.querySelector(".equal");
 
 for (let i = 0; i < values.length; i++) {
     const node = values[i];
@@ -35,6 +36,36 @@ for (let i = 0; i < values.length; i++) {
         display.textContent = displayString;
     });
 }
+
+for (let j = 0; j < operators.length; j++) {
+    const node = operators[j];
+    node.addEventListener("click", () => {
+        firstNumber = displayString;
+        displayString = "";
+        operator = node;
+    });
+}
+
+console.log(equal)
+equal.addEventListener("click", () => {
+    secondNumber = displayString;
+    switch (operator.id) {
+        case "divide":
+            displayString = operate(firstNumber, divide, secondNumber);
+            break;
+        case "multiply":
+            displayString = operate(firstNumber, multiply, secondNumber);
+            break;
+        case "add":
+            displayString = operate(firstNumber, add, secondNumber);
+            break;
+        case "subtract":
+            displayString = operate(firstNumber, subtract, secondNumber);
+            break;
+    }
+    display.textContent = displayString;
+});
+
 
 // buttonNames = []
 // let displayString = "";
